3ea840952bc64ed72bde456dd66d82e30e362015
Денис <59793012+Denisok94@users.noreply.github.com>
2025-08-17 20:55:28 +0300

    [+] Chat:is_forum

--- src/model/Chat.php
+++ src/model/Chat.php
@@ -9,7 +9,7 @@ class Chat
 {
     public int $id;
     /**
-     * private|group
+     * private|group|supergroup|channel
      * @var string
      */
     public string $type;
@@ -17,6 +17,7 @@ class Chat
     public ?string $username = null;
     public ?string $first_name = null;
     public ?string $last_name = null;
+    public bool $is_forum = false;
     public function __construct(array $data)
     {
         $this->id = $data['id'];
@@ -25,5 +26,6 @@ class Chat
         $this->username = $data['username'] ?? null;
         $this->first_name = $data['first_name'] ?? null;
         $this->last_name = $data['last_name'] ?? null;
+        $this->is_forum = $data['is_forum'] ?? false;
     }
 }
